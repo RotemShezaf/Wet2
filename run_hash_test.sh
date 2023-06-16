9f2cf8a2a5aa485d065836d196ca429e903fd6eb
Avi Aaron <81820388+aviRon012@users.noreply.github.com>
2023-06-16 12:12:44 +0300

    test scprit

--- run_hash_test.sh
+++ run_hash_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-g++ -std=c++11 -Wall -Werror -pedantic-errors -DTEST -g hash_test.cpp hashTable.cpp -o hash_test.exe
-chmod +x hash_test.exe
-valgrind --leak-check=full --show-leak-kinds=all ./hash_test.exe 200
+g++ -std=c++11 -Wall -Werror -pedantic-errors -DTEST -g test/hash_test.cpp hashTable.cpp -o test/hash_test.exe
+chmod +x test/hash_test.exe
+valgrind --leak-check=full --show-leak-kinds=all test/hash_test.exe 200
